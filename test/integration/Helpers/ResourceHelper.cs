af663e80a788619ad119b513d8fb85bcf061110a
Laolu Benson <bakinsola@gmail.com>
2019-03-25 20:16:16 +0000

    Add ResourceHelper to return test app bytes

---
+++ test/integration/Helpers/ResourceHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+namespace Appium.Net.Integration.Tests.Helpers
+{
+    public static class ResourceHelper
+    {
+        public static byte[] GetBytes<T>(string name)
+        {
+            var assembly = typeof(T).Assembly;
+            using (var stream = assembly.GetManifestResourceStream(name))
+            {
+                
+                if (stream == null)
+                {
+                    Console.WriteLine("Actual name: " + name);
+                    throw new Exception(
+                        $"Resource {name} not found in {assembly.FullName}. \nValid resources are: {string.Join(", ", assembly.GetManifestResourceNames())}.");
+                }
+                using (var ms = new MemoryStream())
+                {
+                    stream.CopyTo(ms);
+                    return ms.ToArray();
+                }
+            }
+        }
+    }
+}
